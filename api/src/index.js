5de1855e2b676fee0936c72c2439c0dde686bacc
Aleksandar Nachev <nachevalexandar.a@gmail.com>
2025-10-24 17:54:07 +0300

    fix db bug

--- api/src/index.js
+++ api/src/index.js
@@ -6,7 +6,7 @@ import mongoose from 'mongoose'
 const app = express()
 //Setup mongoose
 try{
-    await mongoose.connect('mongodb://localhost:27017', {
+    await mongoose.connect('mongodb://127.0.0.1:27017', {
         dbName:'furniture-sept-2025'
     })
     console.log('Succesfully conected to db');
